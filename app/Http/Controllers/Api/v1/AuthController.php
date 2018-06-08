2acd589452432eabd4eb63b27c41e4c7789d4339
majed <majed@prine.ch>
2018-06-08 20:24:22 +0200

    updated register

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -99,16 +99,17 @@ class AuthController extends Controller
         $email = $request->input('email');
         $password = $hasher->make($request->input('password'));
 
-        $address = isset($request->address)?$request->input('address'):'';
-        $address_addition = isset($request->address_addition)?$request->input('address_addition'):'';
-        $postcode = isset($request->postcode)?$request->input('postcode'):'';
-        $city = isset($request->city)?$request->input('city'):'';
-        $country = isset($request->country)?$request->input('country'):'';
+        // $address = isset($request->address)?$request->input('address'):'';
+        // $address_addition = isset($request->address_addition)?$request->input('address_addition'):'';
+        // $postcode = isset($request->postcode)?$request->input('postcode'):'';
+        // $city = isset($request->city)?$request->input('city'):'';
+        // $country = isset($request->country)?$request->input('country'):'';
 
-        // $address_addition = $request->input('address_addition');
-        // $postcode = $request->input('postcode');
-        // $city = $request->input('city');
-        // $country = $request->input('country');
+        $address = $request->input('address');
+        $address_addition = $request->input('address_addition');
+        $postcode = $request->input('postcode');
+        $city = $request->input('city');
+        $country = $request->input('country');
         
         
         $user = User::create([
